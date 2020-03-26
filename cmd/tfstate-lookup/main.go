a4dbdd302c2f60cd37f21455845d940a8f380eb1
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-03-26 17:26:35 +0900

    add flag -state

--- cmd/tfstate-lookup/main.go
+++ cmd/tfstate-lookup/main.go
@@ -2,13 +2,38 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"log"
 	"os"
 
 	"github.com/fujiwara/tfstate-lookup/tfstate"
+	"github.com/hashicorp/logutils"
 )
 
+func init() {
+	level := os.Getenv("LOG_LEVEL")
+	if level == "" {
+		level = "warn"
+	}
+	filter := &logutils.LevelFilter{
+		Levels:   []logutils.LogLevel{"debug", "warn", "error"},
+		MinLevel: logutils.LogLevel(level),
+		Writer:   os.Stderr,
+	}
+	log.SetOutput(filter)
+}
+
 func main() {
-	s, err := tfstate.Read(os.Stdin)
+	var stateFile string
+	flag.StringVar(&stateFile, "state", "terraform.tfstate", "tfstate file path")
+	flag.Parse()
+
+	f, err := os.Open(stateFile)
+	if err != nil {
+		panic(err)
+	}
+
+	s, err := tfstate.Read(f)
 	if err != nil {
 		panic(err)
 	}
