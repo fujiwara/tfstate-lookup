894ca321facd4f76318dc91136d8b319f2aea0de
FUJIWARA Shunichiro <fujiwara.shunichiro@gmail.com>
2020-03-27 01:10:03 +0900

    add -r (raw) output option

--- cmd/tfstate-lookup/main.go
+++ cmd/tfstate-lookup/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"flag"
+	"fmt"
 	"log"
 	"os"
 
@@ -24,22 +25,51 @@ func init() {
 }
 
 func main() {
-	var stateFile string
+	if err := _main(); err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		os.Exit(1)
+	}
+}
+
+func _main() error {
+	var (
+		stateFile string
+		raw       bool
+	)
 	flag.StringVar(&stateFile, "state", "terraform.tfstate", "tfstate file path")
+	flag.StringVar(&stateFile, "s", "terraform.tfstate", "tfstate file path")
+	flag.BoolVar(&raw, "raw", false, "raw output")
+	flag.BoolVar(&raw, "r", false, "raw output")
 	flag.Parse()
+	if len(flag.Args()) == 0 {
+		flag.Usage()
+		return nil
+	}
 
 	f, err := os.Open(stateFile)
 	if err != nil {
-		panic(err)
+		return err
 	}
 
 	s, err := tfstate.Read(f)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	res, err := s.Lookup(flag.Arg(0))
 	if err != nil {
-		panic(err)
+		return err
 	}
-	json.NewEncoder(os.Stdout).Encode(res)
+	if raw {
+		switch res.(type) {
+		case string, float64:
+			fmt.Fprintln(os.Stdout, res)
+		default:
+			json.NewEncoder(os.Stdout).Encode(res)
+		}
+	} else {
+		enc := json.NewEncoder(os.Stdout)
+		enc.SetIndent("", "  ")
+		enc.Encode(res)
+	}
+	return nil
 }
