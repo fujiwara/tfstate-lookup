490dd7f6be49079d8669acefe472f7ba02a12c6b
Ian Muge <ian.muge@ardoq.com>
2021-08-11 17:52:35 +0200

    Able to query the state with named accessKey env variable, to be changed to CLI auth

--- tfstate/remote_azurerm.go
+++ tfstate/remote_azurerm.go
@@ -1,20 +1,26 @@
 package tfstate
 
 import (
-	//"bytes"
 	"context"
 	"fmt"
+	"github.com/Azure/azure-storage-blob-go/azblob"
 	"io"
 	"log"
 	"net/url"
-	"github.com/Azure/azure-storage-blob-go/azblob"
 	"os"
+	"path"
 )
 
 func readAzureRMState(config map[string]interface{}, ws string) (io.ReadCloser, error) {
-	accountName, containerName, key  := *strp(config["storage_account_name"]), *strpe(config["container_name"]), *strpe(config["key"])
-
-	return readAzureRM(accountName,containerName, key)
+	accountName, containerName, key := *strp(config["storage_account_name"]), *strpe(config["container_name"]), *strpe(config["key"])
+	if ws != defaultWorkspace {
+		if prefix := strp(config["workspace_key_prefix"]); prefix != nil {
+			key = path.Join(*prefix, ws, key)
+		} else {
+			key = path.Join(defaultWorkspeceKeyPrefix, ws, key)
+		}
+	}
+	return readAzureRM(accountName, containerName, key)
 }
 
 func readAzureRM(accountName string, containerName string, key string) (io.ReadCloser, error) {
@@ -22,36 +28,29 @@ func readAzureRM(accountName string, containerName string, key string) (io.ReadC
 	ctx := context.Background()
 	URL, _ := url.Parse(fmt.Sprintf("https://%s.blob.core.windows.net/%s", accountName, containerName))
 
-	accountName, accountKey := os.Getenv("AZURE_STORAGE_ACCOUNT"), os.Getenv("AZURE_STORAGE_ACCESS_KEY")
-	if len(accountName) == 0 || len(accountKey) == 0 {
-		log.Fatal("Either the AZURE_STORAGE_ACCOUNT or AZURE_STORAGE_ACCESS_KEY environment variable is not set")
+	accountKey := os.Getenv("AZURE_STORAGE_ACCESS_KEY")
+	if len(accountKey) == 0 {
+		log.Fatal("AZURE_STORAGE_ACCESS_KEY environment variable is not set")
 	}
-	azblob.NewAnonymousCredential()
 
 	credential, err := azblob.NewSharedKeyCredential(accountName, accountKey)
+
 	//set up client
 	p := azblob.NewPipeline(credential, azblob.PipelineOptions{})
 	containerURL := azblob.NewContainerURL(*URL, p)
 	blobURL := containerURL.NewBlockBlobURL(key)
-
 
 	if err != nil {
 		return nil, err
 	}
 
 	//fetch data
-	downloadResponse, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false,azblob.ClientProvidedKeyOptions{})
-	// NOTE: automatically retries are performed if the connection fails
-	//bodyStream := downloadResponse.Body(azblob.RetryReaderOptions{MaxRetryRequests: 20})
-	//bodyStream := azblob.DownloadBlobToBuffer(ctx, blobURL,0,azblob.CountToEnd,,azblob.BlobAccessConditions{})
-	// read the body into a buffer
-	//downloadedData := bytes.Buffer{}
-	//_, err = downloadedData.ReadFrom(bodyStream)
-
-	r:= downloadResponse.Body(azblob.RetryReaderOptions{MaxRetryRequests: 20})
+	response, err := blobURL.Download(ctx, 0, azblob.CountToEnd, azblob.BlobAccessConditions{}, false, azblob.ClientProvidedKeyOptions{})
 	if err != nil {
 		return nil, err
 	}
+	defer response.Response().Body.Close()
+	r := response.Body(azblob.RetryReaderOptions{MaxRetryRequests: 20})
 
 	return r, nil
 }
