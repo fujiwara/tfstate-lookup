25c77355cb44ef65d0c337bbfae375b2026b600f
Hitesh Patel <hiteshpatel@oncolens.com>
2023-10-05 18:02:19 -0400

    #139 use TFE_TOKEN for state downloads

--- tfstate/remote_http.go
+++ tfstate/remote_http.go
@@ -2,12 +2,16 @@ package tfstate
 
 import (
 	"context"
+	"fmt"
 	"io"
 	"net/http"
+	"os"
 )
 
 func readHTTP(ctx context.Context, u string) (io.ReadCloser, error) {
 	req, err := http.NewRequestWithContext(ctx, http.MethodGet, u, nil)
+	token := os.Getenv("TFE_TOKEN")
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", token))
 	if err != nil {
 		return nil, err
 	}
