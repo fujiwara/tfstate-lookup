992624bcd7a52e1ba9f1ab0fab4db20e74c3d6c7
fujiwara <fujiwara.shunichiro@gmail.com>
2023-10-06 10:08:15 +0900

    add readHTTPWithRequest func.
    can use any http.Request.

--- tfstate/remote_http.go
+++ tfstate/remote_http.go
@@ -11,6 +11,13 @@ func readHTTP(ctx context.Context, u string) (io.ReadCloser, error) {
 	if err != nil {
 		return nil, err
 	}
+	return readHTTPWithRequest(ctx, req)
+}
+
+func readHTTPWithRequest(ctx context.Context, req *http.Request) (io.ReadCloser, error) {
+	if c := req.Context(); c != ctx {
+		req = req.WithContext(ctx)
+	}
 	resp, err := http.DefaultClient.Do(req)
 	if err != nil {
 		return nil, err
